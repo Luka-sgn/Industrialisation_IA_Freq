b52ac8da1987d45ad61412c4f4aff209227e37a0
lukasegouin <segouinluka@gmail.com>
2025-05-09 17:22:19 +0200

    test unitaires corrigés avec unittest

--- test/Model/preprocessing_test.py
+++ test/Model/preprocessing_test.py
@@ -1,10 +1,12 @@
 import unittest
+from unittest.mock import patch
 import pandas as pd
 from src.Model.preprocessing import prepare_data
 
 class TestPreprocessYTrain(unittest.TestCase):
 
-    def test_preprocess_y_train(self):
+    @patch('pandas.DataFrame.to_csv')  # Mocker la méthode to_csv
+    def test_preprocess_y_train(self, mock_to_csv):
         # Exemple de données
         X_train = pd.DataFrame({'ANNEE_ASSURANCE': [1, 2, 3]})
         y_train = pd.DataFrame({'FREQ': [0, 1, 0], 'SINISTRE': [0, 1, 0]})
@@ -15,6 +17,9 @@ class TestPreprocessYTrain(unittest.TestCase):
         # Vérifier que la transformation a bien eu lieu
         self.assertIn('NB_SINISTRES', processed_y_train.columns, "La colonne 'NB_SINISTRES' n'a pas été ajoutée.")
         self.assertEqual(processed_y_train['SINISTRE'].dtype, 'int64', "Le type de la colonne 'SINISTRE' n'est pas 'int64'.")
+
+        # Vérifier que la méthode to_csv n'a pas été appelée
+        mock_to_csv.assert_not_called()
 
 if __name__ == '__main__':
     unittest.main()
