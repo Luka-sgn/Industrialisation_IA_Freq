a87c92b44ba2d898b870ce094f2bef284d5bd8e0
lukasegouin <segouinluka@gmail.com>
2025-05-10 12:54:18 +0200

    add creatin of Class_test dir for github CI pipeline

--- test/data_engineering/preprocessing_test.py
+++ test/data_engineering/preprocessing_test.py
@@ -3,13 +3,12 @@ import pandas as pd
 import os
 from src.data_engineering.preprocessing import preprocess_y_train
 
-
-# Classe de test
 class TestPreprocessYTrain(unittest.TestCase):
 
     def setUp(self):
-        os.makedirs("Classes_test", exist_ok=True)
-        # Crée les DataFrames d'exemple
+        self.test_dir = os.path.join("test", "data_engineering", "Classes_test")
+        os.makedirs(self.test_dir, exist_ok=True)
+
         self.y_train = pd.DataFrame({
             "FREQ": [0.0, 0.1, 0.5, 1.0]
         })
@@ -18,21 +17,21 @@ class TestPreprocessYTrain(unittest.TestCase):
         })
 
     def test_nb_sinistres_calculation(self):
-        path = "/Users/lukasegouin/IdeaProjects/Industrialisation_IA_Freq/test/data_engineering/Classes_test/Y_train_sinistre_2classes_test.csv"
-        result = preprocess_y_train(self.y_train.copy(), self.X_train,path)
+        path = os.path.join(self.test_dir, "Y_train_sinistre_2classes_test.csv")
+        result = preprocess_y_train(self.y_train.copy(), self.X_train, path)
         expected_nb_sinistres = [0.0, 0.5, 1.0, 1.0]
         self.assertListEqual(result["NB_SINISTRES"].tolist(), expected_nb_sinistres)
 
     def test_sinistre_classification(self):
-        path = "/Users/lukasegouin/IdeaProjects/Industrialisation_IA_Freq/test/data_engineering/Classes_test/Y_train_sinistre_2classes_test.csv"
-        result = preprocess_y_train(self.y_train.copy(), self.X_train,path)
+        path = os.path.join(self.test_dir, "Y_train_sinistre_2classes_test.csv")
+        result = preprocess_y_train(self.y_train.copy(), self.X_train, path)
         expected_sinistre = [0, 0, 1, 1]
         self.assertListEqual(result["SINISTRE"].tolist(), expected_sinistre)
 
     def test_csv_output(self):
-        path = "/Users/lukasegouin/IdeaProjects/Industrialisation_IA_Freq/test/data_engineering/Classes_test/Y_train_sinistre_2classes_test.csv"
-        preprocess_y_train(self.y_train.copy(), self.X_train,path)
-        self.assertTrue(os.path.exists("Classes_test/Y_train_sinistre_2classes_test.csv"))
+        path = os.path.join(self.test_dir, "Y_train_sinistre_2classes_test.csv")
+        preprocess_y_train(self.y_train.copy(), self.X_train, path)
+        self.assertTrue(os.path.exists(path))
 
 if __name__ == '__main__':
     unittest.main()
